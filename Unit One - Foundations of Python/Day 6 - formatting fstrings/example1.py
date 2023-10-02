d0fb1ebdab4f5cd6d9653b4777602d48384e0f1b
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-10-02 14:47:36 -0400

    formatting fstrings example 1

---
+++ Unit One - Foundations of Python/Day 6 - formatting fstrings/example1.py
@@ -0,0 +1,19 @@
+average = 12.65675765
+print(f"{average}")     
+print(f"{average:.2f}") 
+print(f"{average:.3f}") 
+
+number = 6.1
+print(f"{number:.2f}")
+
+number = 6
+print(f"{number:.2f}") 
+
+math_mark = 34
+math_total = 45
+
+math_average = math_mark / math_total
+
+print(f"{math_average}")      
+print(f"{math_average:.1%}")  
+print(f"{math_average:.1f}")
