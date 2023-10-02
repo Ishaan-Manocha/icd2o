062a80b0c1551fd3866d11cfd3cda6401088fcd9
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-10-02 15:20:15 -0400

    Homework

---
+++ Unit One - Foundations of Python/Day 6 - formatting fstrings/decimalplaces.py
@@ -0,0 +1,8 @@
+import math
+
+print(f"{math.pi: .3f}")
+
+price = 29.99
+
+print(f"${price}")
+
---
+++ Unit One - Foundations of Python/Day 6 - formatting fstrings/percentagewithdecimal.py
@@ -0,0 +1,7 @@
+tax_rate = 0.075
+
+discount_rate = 0.25
+
+print(f"{tax_rate:.2%}")
+
+print(f"{discount_rate:.1%}")
