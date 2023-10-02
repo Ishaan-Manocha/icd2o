e5943091d0ea2d8887eb333d7e09d24042479f09
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-10-02 14:55:43 -0400

    formatting fstrings eample 2 and 3

---
+++ Unit One - Foundations of Python/Day 6 - formatting fstrings/example2.py
@@ -0,0 +1,15 @@
+print("12345678901234567890")
+
+name = "Steve"
+
+print(f"{name}")
+
+print(f"{name:>20}")
+
+print(f"{name:>10}")
+
+print(f"{name:<10}Dev")
+
+ 
+
+print(f"{name:^20}")
---
+++ Unit One - Foundations of Python/Day 6 - formatting fstrings/example3.py
@@ -0,0 +1,15 @@
+import math
+
+
+average = 95.324532
+
+print(f"{average}")
+
+print(f"{average:.2f}")
+
+print(f"{average:>20}")
+
+print(f"{average:>20.2f}")
+
+
+print(f"{math.pi:>10.2f}")
