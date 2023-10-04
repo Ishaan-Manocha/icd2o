7841ecb486f334795d187b3d59dba4be0dadf3f5
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-10-04 13:54:54 -0400

    Dots and Boxes

---
+++ Unit One - Foundations of Python/Day 8 - Dots and Boxes/example1.py
@@ -0,0 +1,56 @@
+print("Game #1")
+print("Opponent's name: Brandon")
+print("My points: 30")
+print("Opponent's points: 6")
+
+print("Game #2")
+print("Opponent's name: Abraham")
+print("My points: 32")
+print("Opponent's points: 4")
+
+print("Game #3")
+print("Opponent's name: Alex")
+print("My points: 20")
+print("Opponent's points: 16")
+
+print("Game #4")
+print("Opponent's name: Joshua")
+print("My points: 10")
+print("Opponent's points: 26")
+
+print("Game #5")
+print("Opponent's name: Brandon")
+print("My points: 33")
+print("Opponent's points: 3")
+
+item = "Product"
+
+ 
+
+quantity = 3
+
+ 
+
+price = 25.99
+
+ 
+
+total = price * quantity
+
+ 
+
+ 
+
+ 
+
+print(f"{'Item':>5} {'Quantity':>15} {'Price':>16.5} {'Total':>25}")
+
+ 
+
+print(f"{item:>5} {quantity:>10} {price:>20.2f} {total:>25}")
+
+
+print("Summary: ")
+print("Total Points: 131")
+print("Total Opponent Points: 46")
+print("Percentage Points Received: 74.01%")
