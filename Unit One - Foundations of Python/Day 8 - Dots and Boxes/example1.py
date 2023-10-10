9d144ae6a249a0256ad58c4e88715d9f3fa2fe6b
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-10-10 14:03:51 -0400

    Homework

--- Unit One - Foundations of Python/Day 8 - Dots and Boxes/example1.py
+++ Unit One - Foundations of Python/Day 8 - Dots and Boxes/example1.py
@@ -1,54 +1,111 @@
+
+print(str(input("Enter Your Name: ")))
+
+print("")
+
 print("Game #1")
+
 print("Opponent's name: Brandon")
-print("My points: 30")
-print("Opponent's points: 6")
+
+print("Opponent's Points: 6")
+
+print("your point's: 30")
+
+name1 = "Brandon"
+
+oppointsg1 = 6
+
+mypointsg1 = 30
+
+print("")
 
 print("Game #2")
+
 print("Opponent's name: Abraham")
-print("My points: 32")
-print("Opponent's points: 4")
+
+print("Opponent's Points: 4")
+
+print("your point: 32")
+
+name2 = "Abraham"
+
+oppointsg2 = 4
+
+mypointsg2 = 32
+
+print("")
 
 print("Game #3")
+
 print("Opponent's name: Alex")
-print("My points: 20")
-print("Opponent's points: 16")
+
+print("Opponent's Points: 16")
+
+print("your point's: 20")
+
+name3 = "Alex"
+
+oppointsg3 = 16
+
+mypointsg3 = 20
+
+print("")
 
 print("Game #4")
+
 print("Opponent's name: Joshua")
-print("My points: 10")
-print("Opponent's points: 26")
+
+print("Opponent's Points: 20")
+
+print("your point's: 16")
+
+name4 = "Joshua"
+
+oppointsg4 = 20
+
+mypointsg4 = 16
+
+print("")
 
 print("Game #5")
+
 print("Opponent's name: Brandon")
-print("My points: 33")
-print("Opponent's points: 3")
 
-item = "Product"
+print("Opponent's Points: 33")
 
- 
+print("your point's: 3")
 
-quantity = 3
+name5 = "Brandon"
 
- 
+oppointsg5 = 3
 
-price = 25.99
+mypointsg5 = 33
 
- 
+box1 = 30/36 * 100
 
-total = price * quantity
+box2 = 32/36 * 100
 
- 
+box3 = 20/36 * 100
 
- 
+box4 = 10/36 * 100
 
- 
+box5 = 33/36 * 100
 
-print(f"{'Item':>5} {'Quantity':>15} {'Price':>16.5} {'Total':>25}")
+print("")
 
- 
+print(f"{'Opponent' :>7} {'My points' :>14} {'Oponnents points' :>21} {'Box %' :>9}")
 
-print(f"{item:>5} {quantity:>10} {price:>20.2f} {total:>25}")
+print(f"{name1 :>7} {mypointsg1 :>11} {oppointsg1 :>17} {box1 :>29}")
 
+print(f"{name2 :>6} {mypointsg2 :>11} {oppointsg2 :>17} {box2 :>29}")
+
+print(f"{name3 :>2} {mypointsg3 :>14} {oppointsg3 :>17} {box3 :>29}")
+
+print(f"{name4 :>4} {mypointsg4 :>12} {oppointsg4 :>17} {box4 :>29}")
+
+print(f"{name5 :>5} {mypointsg5 :>11} {oppointsg5 :>17} {box5 :>29}")
+
+print("")
 
 print("Summary: ")
 print("Total Points: 131")
