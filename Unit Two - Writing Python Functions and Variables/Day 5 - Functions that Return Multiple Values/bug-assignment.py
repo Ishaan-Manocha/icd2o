b57b028ef96c32960d6640e2dea921f667556dc8
Ishaan Manocha <imanocha@bayviewglen.ca>
2023-11-01 12:29:25 -0400

    Bug Assignment

---
+++ Unit Two - Writing Python Functions and Variables/Day 5 - Functions that Return Multiple Values/bug-assignment.py
@@ -0,0 +1,31 @@
+def input_bug_counts(bug_type):
+    count = int(input(f"Enter the count of {bug_type} bugs: "))
+    return count
+ 
+ 
+def calculate_percent(total, count):
+    percent = (count / total) * 100
+    return percent
+ 
+ 
+def input_bug_type_and_count():
+    bug_type = input("Enter the type of bug: ")
+    count = input_bug_counts(bug_type)
+    return bug_type, count
+ 
+ 
+def display_table(bug1, count1, bug2, count2, bug3, count3):
+    total = count1 + count2 + count3
+    print("Bug Type     Count   Percentage")
+    print("---------------------------------")
+    print(f"{bug1}{count1:>16}{calculate_percent(total, count1):>12.2f}%")
+    print(f"{bug2}{count2:>16}{calculate_percent(total, count2):>12.2f}%")
+    print(f"{bug3}{count3:>16}{calculate_percent(total, count3):>12.2f}%")
+    print("---------------------------------")
+    print("Bug Type      Count       100% ")
+
+bug1_type, bug1_count = input_bug_type_and_count()
+bug2_type, bug2_count = input_bug_type_and_count()
+bug3_type, bug3_count = input_bug_type_and_count()
+ 
+display_table(bug1_type, bug1_count, bug2_type, bug2_count, bug3_type, bug3_count)
